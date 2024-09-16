f9e6a934745396deeaada5f430484f4a3aceddab
Felipe Pedroza <felipelp2910@gmail.com>
2024-09-16 14:03:57 -0300

    Knight

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -1,9 +1,6 @@
 package src.Chess;
 
-import src.Chess.pieces.Bishop;
-import src.Chess.pieces.King;
-import src.Chess.pieces.Pawn;
-import src.Chess.pieces.Rook;
+import src.Chess.pieces.*;
 import src.boardgame.Board;
 import src.boardgame.BoardException;
 import src.boardgame.Piece;
@@ -212,6 +209,8 @@ public class ChessMatch {
         placeNewPiece('f', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('b', 1, new Knight(board, Color.WHITE));
+        placeNewPiece('g', 1, new Knight(board, Color.WHITE));
         placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
 
@@ -227,6 +226,8 @@ public class ChessMatch {
         placeNewPiece('f', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('g', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('h', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('b', 8, new Knight(board, Color.BLACK));
+        placeNewPiece('g', 8, new Knight(board, Color.BLACK));
         placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
 
--- src/Chess/pieces/Knight.java
+++ src/Chess/pieces/Knight.java
@@ -3,17 +3,87 @@ package src.Chess.pieces;
 import src.Chess.ChessPiece;
 import src.Chess.Color;
 import src.boardgame.Board;
+import src.boardgame.Position;
 
 public class Knight extends ChessPiece {
     public Knight(Board board, Color color) {
         super(board, color);
     }
+
     public String toString() {
-        return "Kn";
+        return "N";
+    }
+
+    private boolean canMove(Position position) {
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+        return p == null || p.getColor() != getColor();
     }
 
     @Override
     public boolean[][] possibleMoves() {
-        return new boolean[0][];
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        Position p = new Position(0, 0);
+
+
+        // above right
+        p.setValues(position.getRow() - 2, position.getColum() + 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColum()] = true;
+
+        }
+
+        p.setValues(position.getRow() - 1, position.getColum() + 2);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColum()] = true;
+
+        }
+
+
+
+        // above left
+        p.setValues(position.getRow() - 2, position.getColum() - 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColum()] = true;
+
+        }
+
+        p.setValues(position.getRow() - 1, position.getColum() - 2);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColum()] = true;
+
+        }
+
+
+
+        // below right
+        p.setValues(position.getRow() + 2, position.getColum() + 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColum()] = true;
+
+        }
+
+        p.setValues(position.getRow() + 1, position.getColum() + 2);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColum()] = true;
+
+        }
+
+
+
+        // below left
+        p.setValues(position.getRow() + 2, position.getColum() - 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColum()] = true;
+
+        }
+
+        p.setValues(position.getRow() - 1, position.getColum() + 2);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColum()] = true;
+
+        }
+
+
+        return mat;
     }
 }
